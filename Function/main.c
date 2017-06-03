824e64d50359748d7df24fcd4bca8f48a108329e
Aurèle Oulès <aurele@oules.com>
2017-06-03 13:29:39 +0200

    Added deritative in the Function project

--- Function/main.c
+++ Function/main.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 
-float getImageFunction(float x) {
+float f(float x) {
     // f(x) = (2x + 3)(3x*x + 2)
     return (2*x +3) * (3 * x * x +2);
 }
 
+float getDeritativeFunction(float x, float h) {
+    return (f(x + h) - f(x))/h;
+}
+
 int main() {
     float x, image;
+    float xDeritative, hDeritative, resultDeritative;
     printf("Enter x value: ");
     scanf("%f", &x);
-    image = getImageFunction(x);
-    printf("The image is %f\n", image);
+    image = f(x);
+    printf("The image is %.2f\n\n", image);
+
+    printf("Enter x for the deritative: ");
+    scanf("%f", &xDeritative);
+    printf("Enter h for the deritative: ");
+    scanf("%f", &hDeritative);
+
+    resultDeritative = getDeritativeFunction(xDeritative, hDeritative);
+    printf("The result of the deritative is %.2f\n", resultDeritative);
     return 0;
 }
